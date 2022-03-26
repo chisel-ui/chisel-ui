9035bb1e266126461884bf2e7cb23fa45c19d4e5
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-26 13:10:46 +0100

    starting to add a basic docs layout

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
+	integrations: [],
 });
