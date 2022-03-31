d3e914fcc62f9e894aa7eaa44d213757bfd1fa3f
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-31 21:03:29 +0200

    adding a headless <Toggle> xelement component

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  buildOptions: {
+    site: 'https://chisel-ui.dev'
+  },
   integrations: [],
   vite: {
     ssr: {
