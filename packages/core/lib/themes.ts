a6f5c72fbaf91f4a202a16736032e2af59c3636c
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-06 22:17:43 +0200

    fixing typescript autocomplete

--- packages/core/lib/themes.ts
+++ packages/core/lib/themes.ts
@@ -1,7 +1,14 @@
+import { defaultBreakpoints } from './breakpoints'
+import { defaultGaps } from './gaps'
 import type { Gaps } from './gaps'
 import type { Breakpoints } from './breakpoints'
 
 export interface Theme {
 	gaps?: Gaps
 	breakpoints?: Breakpoints
+}
+
+export const defualtTheme = {
+	breakpoints: defaultBreakpoints,
+	gaps: defaultGaps
 }
